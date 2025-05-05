270b74cdddbeae9a6d0942c9931bf0148c74975b
WladekBBC <147035999+WladekBBC@users.noreply.github.com>
2025-05-05 17:22:19 +0200

    wldkbbc commit

---
+++ rental-api/src/app.module 2.ts
@@ -0,0 +1,27 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+import { UserService } from './user/user.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type:'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: "rental",
+      entities: [User],
+      synchronize: true
+    }),
+    UserModule,
+    AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
