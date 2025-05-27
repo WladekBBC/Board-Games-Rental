9e5561b0cdde245c283d0dcc757e26ab45d9cbaf
WladekBBC <147035999+WladekBBC@users.noreply.github.com>
2025-05-27 10:08:08 +0200

    translation fix

--- front/src/app/rentals/page.tsx
+++ front/src/app/rentals/page.tsx
@@ -132,6 +132,7 @@ export default function RentalsPage() {
 
     addRental(data).then(()=>{
       handleSuccess(language.gameRented)
+      
     }).catch((err: Error)=>{
       console.log(err)
       handleError(err.cause == 406 ? language.rentGameError : language.serverError)
@@ -225,7 +226,7 @@ export default function RentalsPage() {
             disabled={isProcessing}
             className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
           >
-            {isProcessing ? language.gameAdding : language.renting}
+            {isProcessing ? language.gameAdding : language.rentGame}
           </button>
         </form>
       </div>
