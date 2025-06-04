f508608b322b6fdec8332f05faf7dcee1171ef71
TymoteuszMH <tymek.huszcza@gmail.com>
2025-06-04 13:54:13 +0200

    lang button fix

--- front/src/components/Navbar/LanguageDropdown.tsx
+++ front/src/components/Navbar/LanguageDropdown.tsx
@@ -1,12 +1,14 @@
 import { useLang } from "@/contexts/LanguageContext"
 import { Language } from "@/lib/i18n/translations"
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react"
-import { AnimatePresence } from "framer-motion"
+import React from "react";
+import { useState } from "react";
 
 export const LanguageDropdown = () =>{
-    const { currentLang, setLanguage } = useLang()
+    const { currentLang, setLanguage } = useLang();
+    const [show, setShow] = useState(false)
 
     const languages:Language[] = ['pl', 'ua', 'en', 'ja']
+    const listRef = React.createRef<HTMLButtonElement>();
     
     const getLanguageLabel = (lang: string) => {
         switch(lang) {
@@ -19,28 +21,23 @@ export const LanguageDropdown = () =>{
     }
 
     return (
-        <Menu as="div" className="relative inline-block text-left">
-            <div>
-                <MenuButton className="p-2 rounded-md text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-hidden focus-visible:outline-none">
-                    {getLanguageLabel(currentLang)}
-                </MenuButton>
-            </div>
+        <div className="relative inline-block text-center">
+            <button onClick={()=>listRef.current!.focus()} className="p-2 rounded-md text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700">
+                {getLanguageLabel(currentLang)}
+            </button>
 
-            <MenuItems
-                transition={true}
-                className="absolute right-0 z-10 mt-2 w-max shadow bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 origin-top-right rounded-md focus:outline-hidden focus-visible:outline-none transition-all duration-100 ease-in-out">
-                <div className="py-1">
-                {languages.map((lang)=>(
-                    <>
-                        <MenuItem>
-                            <a onClick={()=>setLanguage(lang)} className="block cursor-pointer px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-                                {getLanguageLabel(lang)}
-                            </a>
-                        </MenuItem>
-                    </>
-                ))}
-                </div>
-            </MenuItems>
-        </Menu>
+            <button
+                ref={listRef}
+                onFocus={()=>setShow(true)}
+                onBlur={()=>setShow(false)}>
+                    <span className={`absolute pointer-events-none right-0 z-10 mt-4 w-max shadow bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-md overflow-hidden transition-all duration-100 ease-in-out ${show ? 'max-h-40' : 'max-h-0'}`}>
+                        {languages.map((lang)=>(
+                                <a key={lang} onClick={()=>{setLanguage(lang); setShow(false)}} className={`block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 pointer-events-auto`}>
+                                    {getLanguageLabel(lang)}
+                                </a>
+                        ))}
+                    </span>
+            </button>
+        </div>
     )
 }
