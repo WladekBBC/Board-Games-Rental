f7875eb8857208af99cf4cd5830e15515fa8ad99
WladekBBC <147035999+WladekBBC@users.noreply.github.com>
2025-06-06 17:57:47 +0200

    translations added, animations added

--- front/src/components/Navbar/LanguageDropdown.tsx
+++ front/src/components/Navbar/LanguageDropdown.tsx
@@ -30,7 +30,7 @@ export const LanguageDropdown = () =>{
                 ref={listRef}
                 onFocus={()=>setShow(true)}
                 onBlur={()=>setShow(false)}>
-                    <span className={`absolute pointer-events-none right-0 z-10 mt-4 w-max shadow bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-md overflow-hidden transition-all duration-100 ease-in-out ${show ? 'max-h-40' : 'max-h-0'}`}>
+                    <span className={`absolute pointer-events-none right-0 z-10 mt-4 w-max shadow-lg bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-md overflow-hidden transition-all duration-100 ease-in-out ${show ? 'max-h-40' : 'max-h-0'}`}>
                         {languages.map((lang)=>(
                                 <a key={lang} onClick={()=>{setLanguage(lang); setShow(false)}} className={`block w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-700 pointer-events-auto`}>
                                     {getLanguageLabel(lang)}
--- front/src/components/Navbar/MobileNavbar.tsx
+++ front/src/components/Navbar/MobileNavbar.tsx
@@ -5,6 +5,7 @@ import { MobileRouterLink } from "./MobileRouterLink";
 import { Perms } from "@/interfaces/perms";
 import { UiNavElements } from "./UiNavElements";
 import { LogoutButton } from "./LogoutButton";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface MobileNavProps {
   onLinkClick?: () => void;
@@ -15,30 +16,42 @@ export const MobileNav = ({ onLinkClick }: MobileNavProps) => {
     const { language } = useLang()
     
     return (
-        <DisclosurePanel className="min-[950px]:hidden dark:bg-gray-800 light:bg-white">
-            <div className="space-y-1 px-2 pt-2 pb-3">
-                {user ? (
-                    <>
-                        <MobileRouterLink link='/' text={language.home} onClick={onLinkClick} />
-                        {[Perms.R, Perms.A].includes(permissions) && (
-                            <MobileRouterLink link='/rentals' text={language.rentals} onClick={onLinkClick}/>
-                        )}
-                        {permissions == Perms.A && (
-                            <MobileRouterLink link='/games' text={language.games} onClick={onLinkClick} />
-                        )}
-                        {permissions == Perms.A && (
-                            <MobileRouterLink link='/users' text={language.users} onClick={onLinkClick} />
-                        )}
-                        <div className="pt-4 pb-3 border-t border-gray-700">
-                            <div className="flex items-center px-3">
-                                    <LogoutButton/>
-                            </div>
-                        </div>
-                    </>
-                ) : (
-                    <MobileRouterLink link='/login' text={language.login} onClick={onLinkClick}/>
-                )}
-            </div>
-        </DisclosurePanel>
+        <AnimatePresence>
+                         
+                <DisclosurePanel className="min-[950px]:hidden dark:bg-gray-800 light:bg-white">
+                    <div className="space-y-1 px-2 pt-2 pb-3">
+                        <motion.div
+                            initial={{ opacity: 0, scaleY: 0.8, height: 0 }}
+                            animate={{ opacity: 1, scaleY: 1, height: 'auto' }}
+                            exit={{ opacity: 0, scaleY: 0.8, height: 0 }}
+                            transition={{ duration: 0.2 }}
+                            className="absolute left-0 z-10 mt-1 w-max min-w-full origin-top bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-600 rounded-md shadow-lg overflow-hidden"
+                            >  
+                            {user ? (
+                                <>
+                                    <MobileRouterLink link='/' text={language.home} onClick={onLinkClick} />
+                                    {[Perms.R, Perms.A].includes(permissions) && (
+                                        <MobileRouterLink link='/rentals' text={language.rentals} onClick={onLinkClick}/>
+                                    )}
+                                    {permissions == Perms.A && (
+                                        <MobileRouterLink link='/games' text={language.games} onClick={onLinkClick} />
+                                    )}
+                                    {permissions == Perms.A && (
+                                        <MobileRouterLink link='/users' text={language.users} onClick={onLinkClick} />
+                                    )}
+                                    <div className="pt-4 pb-3 border-t border-gray-700">
+                                        <div className="flex items-center px-3">
+                                                <LogoutButton/>
+                                        </div>
+                                    </div>
+                                </>
+                            ) : (
+                                <MobileRouterLink link='/login' text={language.login} onClick={onLinkClick}/>
+                            )}
+                        </motion.div>
+                    </div>
+                </DisclosurePanel>
+           
+        </AnimatePresence>
     );
 }
