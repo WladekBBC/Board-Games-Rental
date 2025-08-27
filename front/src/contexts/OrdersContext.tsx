234d042455360be20d4d30c118cb5e45136f29f7
WladekBBC <147035999+WladekBBC@users.noreply.github.com>
2025-08-27 14:33:27 +0200

    Update OrdersContext.tsx

--- front/src/contexts/OrdersContext.tsx
+++ front/src/contexts/OrdersContext.tsx
@@ -1,14 +1,40 @@
-import { Method, request } from "@/interfaces/api"
-import { IOrder } from "@/interfaces/order"
-import { ReactNode, useState } from "react"
+import { Method, request } from "@/interfaces/api";
+import { IOrder } from "@/interfaces/order";
+import { Status } from "@/interfaces/statuses";
+import { ReactNode, useState } from "react";
 
+export function OrdersContext({ children }: { children: ReactNode }) {
+  const [orders, setOrders] = useState<IOrder[]>([]);
 
-export function OrdersContext ({children} : {children:ReactNode}){
+  const addOrder = async (order: Partial<IOrder>) => {
+    return request<IOrder>(
+      "order/add",
+      Method.POST,
+      JSON.stringify(order)
+    ).then((newOrder) => {
+      setOrders([...orders, newOrder]);
+    });
+  };
 
-    const [orders, setOrders] = useState<IOrder[]>([])
+  const acceptOrder = async (id: number) => {
+    return request<IOrder>("order/accept" + id, Method.PATCH).then(() => {
+      setOrders(
+        orders.map<IOrder>((order) => {
+          if (order.id == id) order.status = Status.A;
+          return order;
+        })
+      );
+    });
+  };
 
-    const addOrder = async (order: Partial<IOrder>) => {
-        return request<IOrder>('order/add', Method.POST, JSON.stringify(order)).then((newOrder)=>{setOrders([...orders, newOrder])})
-    }
-    
+  const cencelOrder = async (id: number) => {
+    return request<IOrder>("order/accept" + id, Method.PATCH).then(() => {
+      setOrders(
+        orders.map<IOrder>((order) => {
+          if (order.id == id) order.status = Status.C;
+          return order;
+        })
+      );
+    });
+  };
 }
