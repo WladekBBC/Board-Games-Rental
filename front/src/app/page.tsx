9eb5fc7baa94803236ad1f9a76828a22c6062bdd
WladekBBC <147035999+WladekBBC@users.noreply.github.com>
2025-09-10 18:24:01 +0200

    Update page.tsx

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,26 +1,26 @@
-'use client'
+"use client";
 
-import { useLang } from '@/contexts/LanguageContext'
-import HomeUserPanel from '@/components/HomePage/HomeUserPanel'
-import GameList from '@/components/HomePage/GameList'
-import { Spinner } from '@/components/Messages/Spinner'
-import { useLoading } from '@/contexts/LoadingContext'
+import { useLang } from "@/contexts/LanguageContext";
+import HomeUserPanel from "@/components/HomePage/HomeUserPanel";
+import GameList from "@/components/HomePage/GameList";
+import { Spinner } from "@/components/Messages/Spinner";
+import { useLoading } from "@/contexts/LoadingContext";
 
 /**
  * Home page
  * @returns {React.ReactNode}
  */
 export default function Home() {
-  const { language } = useLang()
-  const { loading } = useLoading()
-  
-  if(!loading)
+  const { language } = useLang();
+  const { loading } = useLoading();
+
+  if (!loading)
     return (
       <main className="container mx-auto px-4 py-8">
         <h1 className="text-3xl font-bold mb-8">{language.appTitle}</h1>
         <HomeUserPanel />
         <GameList />
       </main>
-    )
-  return <Spinner/>
+    );
+  return <Spinner />;
 }
